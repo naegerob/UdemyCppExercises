e9c70f74d5f9299ab14e7f39fd75297156fca19d
naegerob <naeger.robert@gmail.com>
2022-12-14 08:38:06 +0100

    Update main.cc

--- 06_String/SplitWords/Exercise/main.cc
+++ 06_String/SplitWords/Exercise/main.cc
@@ -15,8 +15,8 @@ void clean_text(std::string &text);
 
 void replace_all(std::string &text, std::string_view what, std::string_view with);
 
-template <typename T>
-void split_text(const std::string &text, char delimiter, T result);
+//template <typename T>
+//void split_text(const std::string &text, char delimiter, T result);
 
 std::vector<std::string> split_text(const std::string &text, char delimiter);
 
@@ -29,8 +29,8 @@ int main()
     clean_text(text);
     std::cout << text << '\n' << '\n';
 
-    //const auto splittedText = split_text(text, ' ');
-    //print_vector(splittedText);
+    const auto splittedText = split_text(text, ' ');
+    print_vector(splittedText);
 
     return 0;
 }
@@ -57,9 +57,18 @@ void clean_text(std::string &text)
     }
 }
 
-void replace_all(std::string &text, std::string_view what, std::string_view with);
 
-template <typename T>
-void split_text(const std::string &text, char delimiter, T result);
 
-std::vector<std::string> split_text(const std::string &text, char delimiter);
+std::vector<std::string> split_text(const std::string &text, char delimiter)
+{
+    std::vector<std::string> wordlist;
+    std::string word = "";
+
+    std::cout << word << std::endl;
+    do
+    {
+        word = text.substr(delimiter);
+    }while(word != "");
+    
+    std::cout << word << std::endl;
+}
