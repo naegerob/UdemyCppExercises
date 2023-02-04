78d4c181731b658c140273d12b858bdb30884d52
naeger <naeger.robert@gmail.com>
2023-02-04 13:56:40 +0100

    Update Main.cc

--- 10_Templates/UnitsLibrary/Exercise/Main.cc
+++ 10_Templates/UnitsLibrary/Exercise/Main.cc
@@ -7,7 +7,6 @@ int main()
     const float x = 4500;
     const float y = 4.0F;
 	const auto value = ms_to_s<float, int>(x);
-	//std::cout << ms_to_s(x) << "\n";
 
     const auto deg = 10.0F;
     const auto rad = deg_to_rad(deg);
