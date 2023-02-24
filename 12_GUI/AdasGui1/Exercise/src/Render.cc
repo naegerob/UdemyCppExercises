b1ee83c824351396efa9bf9cde6089f08dead288
naegerob <naeger.robert@gmail.com>
2023-02-24 14:58:46 +0100

    set windows size and rnd data

--- 12_GUI/AdasGui1/Exercise/src/Render.cc
+++ 12_GUI/AdasGui1/Exercise/src/Render.cc
@@ -38,13 +38,46 @@ void plot_table(const VehicleType &ego_vehicle, const NeighborVehiclesType &vehi
     (void)vehicles;    // to avoid warning
 
     const auto num_cols = std::size_t{ 5 };
+	const auto num_rows = std::size_t{ 5 };
+
+	/// TODO: Add code here
+	ImGui::SetNextWindowPos(ImVec2(0.0F, BELOW_LANES));
+	ImGui::SetNextWindowSize(ImVec2(VEHICLE_TABLE_WIDTH, VEHICLE_TABLE_HEIGHT));
 
     if (ImGui::Begin("TableWindow", nullptr, WINDOW_FLAGS_CLEAN))
     {
         if (ImGui::BeginTable("Table", num_cols, TABLE_FLAGS))
         {
+			ImGui::TableSetupColumn("Pos");
+			ImGui::TableSetupColumn("Id");
+			ImGui::TableSetupColumn("Lane");
+			ImGui::TableSetupColumn("Distance");
+			ImGui::TableSetupColumn("Speed");
+			ImGui::TableHeadersRow();
 
-            ImGui::EndTable();
+			// Table data
+			for (int i = 0; i < num_cols; i++)
+			{
+				
+				ImGui::TableNextRow();			// Switch to next row
+				ImGui::TableSetColumnIndex(0); // Set row in column
+				//ImGui::TableNextRow();
+				ImGui::Text("Ego");
+				//ImGui::TableNextRow();
+				ImGui::TableSetColumnIndex(1);
+				ImGui::Text("%d", 2);
+				//ImGui::TableNextRow();
+				ImGui::TableSetColumnIndex(2);
+				ImGui::Text("%d", 77);
+				//ImGui::TableNextRow();
+				ImGui::TableSetColumnIndex(3);
+				ImGui::Text("%.2f", 3.6);
+				//ImGui::TableNextRow();
+				ImGui::TableSetColumnIndex(4);
+				ImGui::Text("%.2f", 8.5);
+			}
+
+			ImGui::EndTable();
         }
 
         ImGui::End();
